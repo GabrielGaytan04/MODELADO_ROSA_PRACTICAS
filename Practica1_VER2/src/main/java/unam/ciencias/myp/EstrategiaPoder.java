2391736112f3a1981088bbbdc818cfc83019d6e0
gossbosther <gossbosther@ciencias.unam.mx>
2026-03-01 17:31:53 -0600

    Agregando acceso interfaz EstrategiaPoder

--- Practica1_VER2/src/main/java/unam/ciencias/myp/EstrategiaPoder.java
+++ Practica1_VER2/src/main/java/unam/ciencias/myp/EstrategiaPoder.java
@@ -2,6 +2,6 @@
  * Estrategia: Interfaz común para la familia de algoritmos (Poderes).
  */
 public interface EstrategiaPoder {
-    void ocupa();
-    void aplicarPoder(Personaje p); // Inyecta los stats al personaje
+    public void ocupa();
+    public void aplicarPoder(Personaje p); // Inyecta los stats al personaje
 }
